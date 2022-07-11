8c8debc00c8e67fc8098bf8d51efcc4d3621750f
praveen021097 <96250697+praveen021097@users.noreply.github.com>
2022-07-11 15:11:12 +0530

    sdsfd

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import { Button } from "./Components/Button";
 import { Pagination } from "./Components/Pagination";
@@ -9,7 +10,8 @@ function App() {
   2. Create the custom components, inside the respective files of Button.jsx and Pagination.jsx
   3. The application should function properly after the implimentation of hooks and Custom components
   */
-  const { count, incCount, decCount } = useCounter({
+ 
+  const { count, incCount, decCount,disable } = useCounter({
     initialValue: 10,
     minValue: 5,
     maxValue: 15,
@@ -18,10 +20,10 @@ function App() {
     <div className="App">
       {/* DO NOT CHANGE the 5 lines below   */}
       <h3>Value: {count}</h3>
-      <button onClick={() => incCount()}>Increment</button>
-      <button onClick={() => incCount(3)}>Increment 3</button>
-      <button onClick={() => decCount(2)}>Decrement 2</button>
-      <button onClick={() => decCount(4)}>Decrement 4</button>
+      <button onClick={() => incCount()} disabled={disable}>Increment</button>
+      <button onClick={() => incCount(3)} disabled={disable} >Increment 3</button>
+      <button onClick={() => decCount(2)} disabled={disable}>Decrement 2</button>
+      <button onClick={() => decCount(4)} disabled={disable}>Decrement 4</button>
 
       <br />
       {/* You can pass the required props as mentioned in the questions to
--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,1 +1,53 @@
-export const useCounter = (init) => {};
+import { useState } from "react";
+
+export const useCounter = (init) => {
+    const [count,setCount]= useState(init.initialValue);
+ const [disable,setDisable]=useState(false);
+ 
+
+
+ const incCount=(value)=>{
+  
+  if(value){
+    if(count<init.maxValue){
+      setDisable(false)
+      setCount(count + value)
+    }
+    else{
+      setDisable(true)
+     
+    }
+    
+
+  }
+  else{
+    value=1;
+    if(count<init.maxValue){
+      setDisable(false)
+      setCount(count + value)
+    }
+    else{
+      setDisable(true)
+      
+    }
+   
+  }
+
+
+ }
+
+ const decCount=(value)=>{
+  
+     
+      if(count>init.minValue){
+        setCount(count-value);
+        setDisable(false)
+       }
+       else{
+        setDisable(true)
+       }
+
+     
+ }
+return {count,incCount,decCount,disable};
+};
